244cea002b10ad79ec3eff2694974697e53d844d
Rocketman35 <harhyom9189@gmail.com>
2024-06-27 13:40:13 +0900

    add test github action

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe("Default test set", () => {
+  it("test1 should be passed1", () => {
+    console.log("test1 passed");
+  });
+
+  it("test2 should be passed2", () => {
+    console.log("test2 passed");
+  });
+});
